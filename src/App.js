680eb63e8326cc3b9df1c2e97f0aa3f56b066462
Andrea <a.carreocoppo2@gmail.com>
2022-02-04 01:43:25 -0300

    added likes function

--- src/App.js
+++ src/App.js
@@ -1,95 +1,126 @@
-import {useEffect, useState} from 'react';
-import {firestore} from './firebase';
+import { useEffect, useState } from 'react';
+import { firestore } from './firebase';
+import './App.css';
+// import heart from '../public/heart.svg';
 
 const App = () => {
   //const [alarm, setAlarm] = useState([]);
   const [tweets, setTweets] = useState([]);
   const [body, setBody] = useState({});
+  const [hasUpdate, setUpdate] = useState(false);
 
+  //mostrar tweets
   const getAllTweets = () => {
     firestore
-    .collection("tweets")
-    //.limit(1) //limitar cuantos tweets mostrar
-    //.where('likes','>',5) //mostrar solo tweets mayores de 5 likes
-    //.orderBy('user','desc') // ordenar de manera desc
-    //.orderBy('likes','asc').startAt(0).endAt(1) // que muestre los de 0 a 1 likes
-    .get()
-    .then((snapshot) => {
-      const tweets = snapshot.docs.map((doc) =>{
-        return {
-          tweet: doc.data().message,
-          autor: doc.data().user,
-          id: doc.id
-        };
+      .collection("tweets")
+      //.limit(1) //limitar cuantos tweets mostrar
+      //.where('likes','>',5) //mostrar solo tweets mayores de 5 likes
+      //.orderBy('user','desc') // ordenar de manera desc
+      //.orderBy('likes','asc').startAt(0).endAt(1) // que muestre los de 0 a 1 likes
+      .get()
+      .then((snapshot) => {
+        const tweets = snapshot.docs.map((doc) => {
+          return {
+            tweet: doc.data().message,
+            autor: doc.data().user,
+            likes: doc.data().likes || 0,
+            id: doc.id
+          };
+        });
+        setTweets(tweets);
       });
-      setTweets(tweets);
-  });
-};
+  };
 
+  //crear tweets
   const createTweet = (e) => {
     e.preventDefault();
     firestore.collection("tweets")
-    .add(body)
-    .then(()=> {
-      getAllTweets()
-    })
-    .catch(err=> console.error(err.message))
+      .add(body)
+      .then(() => {
+        getAllTweets()
+      })
+      .catch(err => console.error(err.message))
   }
 
-   const deleteTweet = (id) => {
+  //borrar tweets
+  const deleteTweet = (id) => {
     firestore.collection("tweets")
-    .doc(id)
-    .delete()
-    .then(()=>{
-      getAllTweets()
-    })
-    .catch(err=> console.error(err.message))
-
+      .doc(id)
+      .delete()
+      .then(() => {
+        getAllTweets()
+      })
+      .catch(err => console.error(err.message))
   }
- 
-useEffect(() => {
-  getAllTweets()
-}, []);
 
+  //actualizar likes tweets
+  const likeTweet = (tweet) => {
+    firestore.doc(`tweets/${tweet.id}`)
+      .update({ likes: tweet.likes + 1 })
+      .then(() => {
+        //getAllTweets()
+      })
+      .catch(err => console.error(err.message))
+  }
+
+  useEffect(() => {
+    //getAllTweets()
+    const desuscribir = firestore.collection("tweets").onSnapshot(snapshot => {
+      const tweets = snapshot.docs.map(doc => {
+        return {
+          tweet: doc.data().message,
+          autor: doc.data().user,
+          likes: doc.data().likes || 0,
+          id: doc.id
+        }
+      })
+      setTweets(tweets);
+    });
+    return () => desuscribir();
+  }, []);
 
   const handleChange = (e) => {
     let newTweet = {
       ...body,
       [e.target.name]: e.target.value
-  };
-  setBody(newTweet)
-}
+    };
+    setBody(newTweet)
+  }
 
   return (
     <div>
-
       <form onSubmit={createTweet}>
         <textarea
-        autoComplete="off"
-        //value={body.message}
-        onChange={handleChange}
-        placeholder="Escribe un tweet"
-        name="message"
-        type="text"
+          autoComplete="off"
+          //value={body.message}
+          onChange={handleChange}
+          placeholder="Escribe un tweet"
+          name="message"
+          type="text"
         />
         <br />
-       <input
-         placeholder="persona autora"
-         //value={body.user}
-         onChange={handleChange}
-         name="user"
-         type="text"
-       />
-       <button>Enviar tweet</button>
+        <input
+          placeholder="Autor"
+          //value={body.user}
+          onChange={handleChange}
+          name="user"
+          type="text"
+        />
+        <button>Enviar</button>
       </form>
 
       {tweets.map((tweet) => {
-        return (   
-          <div key={tweet.id}>
-            <p>{tweet.tweet}</p>
-            <p>Escrito por: {tweet.autor}</p>
-            <button onClick={()=> deleteTweet(tweet.id)}>X</button>
-            <hr/>
+        return (
+          <div className="app" key={tweet.id}>
+            <p>Tweet: {tweet.tweet}</p>
+            <p>Autor: {tweet.autor}</p>
+            <button onClick={() => setUpdate(!hasUpdate)}>Editar</button>
+            <button onClick={() => deleteTweet(tweet.id)}>Borrar</button>
+            <div>
+              <button onClick={() => likeTweet(tweet)}>Me gusta</button>
+              <span>Likes: {tweet.likes}</span>
+            </div>
+            <hr />
           </div>
         )
       })}
