aa9564cba925e4e426a421b17879a6d8af03f6ae
Andrea <a.carreocoppo2@gmail.com>
2022-02-24 19:30:25 -0300

    Solucionando error logo.png

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ProtectedContext from './context/Protected'
 
 
 ReactDOM.render(
   <React.StrictMode>
-  <BrowserRouter>
-    <ProtectedContext>
-      <App />
-    </ProtectedContext>
+    <BrowserRouter>
+      <ProtectedContext>
+        <App />
+      </ProtectedContext>
     </BrowserRouter>
-    </React.StrictMode>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
